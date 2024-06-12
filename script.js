8fd7dbf6bd8e8e81085ced4062d1c5d4d806405e
Willian Gabriel <64757536+meydson@users.noreply.github.com>
2024-06-12 19:15:09 -0300

    exercicio_3

---
+++ script.js
@@ -0,0 +1,9 @@
+function funcao() {
+    var valor1 = prompt("Informe um valor");
+
+    if (parseInt(valor1) >= 10) {
+        alert("O valor informado é maior ou igual a 10!");
+    } else (
+        alert("O valor informado é menor que 10!")
+    )
+}
