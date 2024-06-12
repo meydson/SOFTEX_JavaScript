68211e29d2817eb4760fbf2fb780b373e101286e
Willian Gabriel <64757536+meydson@users.noreply.github.com>
2024-06-12 19:26:11 -0300

    Switch
    introdução ao comando switch

--- script.js
+++ script.js
@@ -7,3 +7,46 @@ function funcao() {
         alert("O valor informado é menor que 10!")
     )
 }
+
+function mes() {
+    var mes = prompt("Informe um número de 1 à 12 referente a um mês do ano");
+
+    switch (parseInt(mes)) {
+        case 1:
+            alert("Janeiro");
+            break
+        case 2:
+            alert("Fevereiro");
+            break
+        case 3:
+            alert("Março");
+            break
+        case 4:
+            alert("Abril");
+            break
+        case 5:
+            alert("Maio");
+            break
+        case 6:
+            alert("Junho");
+            break
+        case 7:
+            alert("Julho");
+            break
+        case 8:
+            alert("Agosto");
+            break
+        case 9:
+            alert("Setembro");
+            break
+        case 10:
+            alert("Outubro");
+            break
+        case 11:
+            alert("Novembro");
+            break
+        case 12:
+            alert("Dezembro");
+            break
+    }
+}
